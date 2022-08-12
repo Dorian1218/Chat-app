9022c1e353b88032d3da1ef7e393b7570f1d9869
Dorian1218 <dmicthell1663@gmail.com>
2022-08-12 15:09:04 -0400

    initialized repository

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require('cors');
+const { Console } = require('console');
+
+app.use(cors());
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected", socket.id)
+
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log("User With ID " + socket.id + " Joined Room " + data)
+    })
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id)
+    })
+})
+
+server.listen(3001, () => {
+    console.log("SERVER IS RUNNING")
+})

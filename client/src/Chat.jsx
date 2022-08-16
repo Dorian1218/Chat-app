55b2a00530bd06602055c675482512d7bb6bb486
Dorian1218 <dmicthell1663@gmail.com>
2022-08-16 15:22:13 -0400

    made main jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Chat({socket, username, room}) {
   return (
-    <div>Chat</div>
+    <div>
+        <div className="chat-header"></div>
+        <div className="chat-bodyr"></div>
+        <div className="chat-footer"></div>
+    </div>
   )
 }
 
